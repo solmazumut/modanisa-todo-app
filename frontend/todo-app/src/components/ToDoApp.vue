1527f0c992a2ff5556f63f250e56816c2e422cdb
umutsolmaz <umutsolmaz00@gmail.com>
2021-12-26 23:45:45 +0300

    UI tests - GREEN

--- frontend/todo-app/src/components/ToDoApp.vue
+++ frontend/todo-app/src/components/ToDoApp.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="ToDoApp">
-    <h1>{{msg}}</h1>
+  <div class="container">
+    <h1 class="text-center mt-3">Modanisa ToDo Case</h1>
+
+    <!-- Enter to do -->
+    <div class="d-flex">
+      <input id="input-box" v-model="todo" type="text" placeholder="Enter To Do" class="form-control">
+      <button id="add-button" class="btn btn-success">ADD</button>
+    </div>
+
+    <!--ToDos -->
+    <div class="todo-table">
+      <table id="to-do-table"  class="table table-striped">
+        <thead>
+        <tr>
+          <th scope="col">ToDo Number</th>
+          <th scope="col">ToDo</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr >
+          <th>1</th>
+          <td>test</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="text-center">
+      <button id="reset-button" class="btn btn-danger" >RESET</button>
+    </div>
+
   </div>
 </template>
 
